013cd0d2dc7c34304696a4138d7c72e680987daa
Akobir <akobir.abduganiev@ya.ru>
2022-05-16 11:37:38 +0500

    SwaggerConfig added!

--- src/main/java/com/company/controller/AuthController.java
+++ src/main/java/com/company/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.company.dto.AuthDTO;
 import com.company.dto.ProfileDTO;
 import com.company.service.AuthService;
 import com.company.service.ProfileService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import javax.validation.Valid;
 @Slf4j
 @RestController
 @RequestMapping("auth/")
+@Api("Login and registration")
 public class AuthController {
     @Autowired
     private AuthService authService;
@@ -21,6 +24,7 @@ public class AuthController {
     private ProfileService profileService;
 
     @PostMapping("/login")
+    @ApiOperation(notes = "This method for login", value = "This method for login")
     public ResponseEntity<?> create(@RequestBody @Valid AuthDTO dto) {
         log.info("Authorization: {}", dto);
         return ResponseEntity.ok(authService.login(dto));
